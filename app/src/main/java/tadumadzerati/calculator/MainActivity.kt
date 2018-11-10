c605f2c227b6ca22538379c79507ffd87699d361
tadumadzerati <44522435+tadumadzerati@users.noreply.github.com>
2018-11-10 15:13:14 +0400

    Update

--- app/src/main/java/tadumadzerati/calculator/MainActivity.kt
+++ app/src/main/java/tadumadzerati/calculator/MainActivity.kt
@@ -2,11 +2,19 @@ package tadumadzerati.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonsClick(view: View) {
+        val button: Button = view as Button
+        resultTextView.text = resultTextView.text.toString() + button.text.toString()
     }
 }
